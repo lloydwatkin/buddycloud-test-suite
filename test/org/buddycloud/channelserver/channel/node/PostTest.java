b3db3339c4a7fa87ed4920b2f985a1b6526c0e63
Lloyd Watkin <lloyd.watkin@surevine.com>
2012-08-10 13:24:43 +0100

    Add some debug

--- test/org/buddycloud/channelserver/channel/node/PostTest.java
+++ test/org/buddycloud/channelserver/channel/node/PostTest.java
@@ -19,7 +19,8 @@ public class PostTest
     {
     	Packet packet = getPacket("resources/channel/node/create-post.request");
 		Packet reply  = sendPacket(packet);
-
+		System.out.println("---------------- received");
+System.out.println(reply);
 		Assert.assertEquals(packet.getPacketID(), getValue(reply, "/iq/@id"));
 		Assert.assertTrue(exists(reply, "/iq/pubsub/publish/item[@id]"));
 		Assert.assertTrue(exists(reply, "/iq/pubsub/publish[@node]"));
