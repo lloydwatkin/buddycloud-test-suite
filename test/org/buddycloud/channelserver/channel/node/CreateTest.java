7051c4ff9e34a4f796a68cda951c09db24a57298
Lloyd Watkin <lloyd.watkin@surevine.com>
2012-08-13 14:25:53 +0100

    Point to correct file

--- test/org/buddycloud/channelserver/channel/node/CreateTest.java
+++ test/org/buddycloud/channelserver/channel/node/CreateTest.java
@@ -3,12 +3,14 @@ package org.buddycloud.channelserver.channel.node;
 import org.buddycloud.channelserver.ChannelServerTestHelper;
 import org.buddycloud.channelserver.TestPacket;
 import org.jivesoftware.smack.packet.Packet;
+import org.junit.Test;
 
 public class CreateTest extends ChannelServerTestHelper
 {
+	@Test
     public void testCanCreateNode() throws Exception
     {
-    	TestPacket packet = getPacket("resources/channel/node/node-create.request");
+    	TestPacket packet = getPacket("resources/channel/node-create.request");
     	packet.setVariable("$NODE", "post");
 		Packet reply  = sendPacket(packet);
 		System.out.println("Channel create");
