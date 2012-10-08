b95124ab23405c54320778b9ab30af961ef3d6ad
Lloyd Watkin <lloyd.watkin@surevine.com>
2012-10-08 12:58:34 +0100

    Add subscrptions node item retrieval test

---
+++ src/test/java/org/buddycloud/channelserver/channel/node/SubscriptionsNodeTest.java
@@ -0,0 +1,43 @@
+package org.buddycloud.channelserver.channel.node;
+
+import junit.framework.Assert;
+
+import org.apache.log4j.Logger;
+import org.buddycloud.channelserver.ChannelServerTestHelper;
+import org.jivesoftware.smack.packet.Packet;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Lloyd Watkin <lloyd.watkin@surevine.com>
+ * 
+ */
+public class SubscriptionsNodeTest extends ChannelServerTestHelper {
+	
+	private static final Logger LOGGER = Logger.getLogger(SubscriptionsNodeTest.class);
+
+	@Test
+	public void testNotHavingAnySubscriptionsReturnsAsExpected() throws Exception {
+
+		Packet packet = getPacket("resources/channel/node/subscription-item-retrieval/success.request", 2);
+		Packet reply = sendPacket(packet, 2);
+		
+		// Get node name
+		System.out.println(reply.toXML());
+		String node = getValue(reply, "/iq/pubsub/items/@node");
+
+		Assert.assertEquals(packet.getPacketID(), getValue(reply, "/iq/@id"));
+		Assert.assertEquals("result", getValue(reply, "/iq/@type"));
+	}
+
+	@Test
+	@Ignore("Not coded yet")
+	public void testSubscriptionItemsAreReturnedCorrectly() throws Exception {
+		
+		Packet packet = getPacket("resources/channel/node/subscription-item-retrieval/success.request");
+		Packet reply = sendPacket(packet);
+		
+		////subscribeToNode.setVariable("$NODE",  node);
+		//sendPacket(subscribeToNode, 2);
+	}
+}
